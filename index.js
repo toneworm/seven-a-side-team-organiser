a59e72da826f9d35b6ed33c6b01f29956a22e165
Rich Williams <rich@toneworm.com>
2018-05-27 11:49:31 +0100

    added process.env.PORT

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var app = express();
 var staticPath = path.join(__dirname, '/src');
 app.use(express.static(staticPath));
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('listening');
 });
