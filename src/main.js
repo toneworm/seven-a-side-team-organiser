13a015fc4756d0a373224f67d97ca99ac4a61bcd
Rich Williams <rich@toneworm.com>
2018-05-27 16:28:14 +0100

    selected, players, subs list, pitch bg and styles

--- src/main.js
+++ src/main.js
@@ -49,14 +49,16 @@ const players = [
 ;(function($, $$){
 
   const numRows = 7
+  const wrapper = $('.wrapper')
   const field = $('.field')
   const subs = $('.subs')
   const saveUrlBtn = $('.save-url-btn')
   const saveUrlInput = $('.save-url-input')
+  const teamInfoSelected = $('.team-info-selected')
+  const teamInfoPlayers = $('.team-info-players')
+  const teamInfoSubs = $('.team-info-subs')
   const href = window.location.href
   const baseUrl = href.indexOf('?') < 0 ? href : href.slice(0, href.indexOf('?'))
-
-  console.log(baseUrl)
 
   // generate rows and players
   generateRows()
@@ -65,10 +67,14 @@ const players = [
   // check url for code
   setUpPlayers()
 
+  // team roster
+  populateTeamInfo()
+
   // drag set up
   const rows = Array.from($$('.row')).concat(subs)
-  dragula(rows, {
+  const drake = dragula(rows, {
     accepts: function(el, target) {
+
       if (target.classList.contains('gk') && target.children.length > 0) {
         return false
       }
@@ -77,12 +83,45 @@ const players = [
     }
   })
 
-  // handle save url click
+  // click handlers
+  drake.on('dragend', populateTeamInfo)
   saveUrlBtn.addEventListener('click', populateUrl)
+  saveUrlInput.addEventListener('click', (e) => e.target.select())
+  wrapper.addEventListener('mouseover', (e) => setSelected(e.target))
+  wrapper.addEventListener('mouseout', (e) => setSelected())
 
   // functions
+  function populateTeamInfo() {
+    const players = []
+    const subs = []
+    $$('.blob').forEach((player) => {
+      if (player.parentNode.classList.contains('row')) {
+        players.push(player.dataset.name)
+      } else {
+        subs.push(player.dataset.name)
+      }
+    })
+
+    if (players.length) {
+      teamInfoPlayers.innerHTML = `<ul><li>${players.join('</li><li>')}</li></ul>`
+    }
+
+    if (subs.length) {
+      teamInfoSubs.innerHTML = `<ul><li>${subs.join('</li><li>')}</li></ul>`
+    }
+  }
+
+  function setSelected(target) {
+    if (target && target.classList.contains('blob')) {
+      teamInfoSelected.innerHTML = target.dataset.name
+    } else {
+      teamInfoSelected.innerHTML = ''
+    }
+  }
+
   function populateUrl() {
-    saveUrlInput.value = `${baseUrl}?code=${getCode()}`
+    const code = getCode()
+    saveUrlInput.value = `${baseUrl}${code ? `?code=${code}` : '' }`
   }
 
   function getChildIndex(parent, el) {
@@ -99,9 +138,7 @@ const players = [
       }
       return playersArr
     }, [])
-
-    console.log(playersList)
-
+    
     playersList.sort((a, b) => a.slice(1) > b.slice(1))
 
     return playersList.map((player) => player.slice(0,2)).join('')
